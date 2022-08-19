8c939fe79a45edd8fc41d4864ae0a089a7ab3646
markapiwat <111260577+markapiwat@users.noreply.github.com>
2022-08-19 21:36:06 +0700

    Edit controller

--- sso/src/main/java/com/pcc/sso/usercontroller/SsoUserController.java
+++ sso/src/main/java/com/pcc/sso/usercontroller/SsoUserController.java
@@ -2,9 +2,11 @@ package com.pcc.sso.usercontroller;
 
 
 import java.util.List;
-
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.CannotCreateTransactionException;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,12 +14,17 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
-
+import com.pcc.sso.handler.GlobalExceptionHandler;
+import com.pcc.sso.response.ResponseHandler;
+import com.pcc.sso.response.SsoUserResponse;
 import com.pcc.sso.user.SsoUser;
 import com.pcc.sso.userrepository.SsoUserRepository;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+
+
+@EnableSwagger2
 @RestController
 @RequestMapping(path = "api/test_sso/")
 public class SsoUserController {
@@ -31,10 +38,28 @@ public class SsoUserController {
     }
 
     
-    @PostMapping("sso_user_test" )
-	public SsoUser addUser( @RequestBody SsoUser user ) {
-        return userRepo.save(user);
-	}
+        @PostMapping("gettoken")
+    public ResponseEntity<SsoUserResponse> addUser( @RequestBody SsoUser users , ResponseHandler handler) {
+        
+
+        SsoUserResponse res = handler.SaveComplete(users);
+
+        try { 
+            
+            userRepo.save(users); 
+
+            }catch (DataAccessException dex) {
+
+                return new GlobalExceptionHandler().addException(dex, users);
+
+            }catch (CannotCreateTransactionException ex){
+
+                return new GlobalExceptionHandler().dbNotFoundException(ex, users);
+
+            }
+
+        return new ResponseEntity<>(res,HttpStatus.OK);
+	    }
 
     
 
